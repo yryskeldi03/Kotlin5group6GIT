d9f199bc84e28fd9887760cdf2c210d819fe52e5
ymajrambekov@gmail.com <ymajrambekov@gmail.com>
2022-12-09 20:44:14 +0600

    second_branch initial commit

--- app/src/main/java/com/yrys/presentation/ui/fragments/NoteListFragment.kt
+++ app/src/main/java/com/yrys/presentation/ui/fragments/NoteListFragment.kt
@@ -27,7 +27,7 @@ class NoteListFragment : BaseFragment(R.layout.fragment_note_list) {
     }
     private val viewModel by viewModels<NoteListViewModel>()
     private val binding by viewBinding(FragmentNoteListBinding::bind)
-    private val a = 0
+    private val second_branch = 0
     private val s = 2
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
